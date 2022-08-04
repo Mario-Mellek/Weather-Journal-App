e663bebf8d8f1de82ead8de59bdf1faa465f56c4
Mario-Mellek <107096218+Mario-Mellek@users.noreply.github.com>
2022-08-04 19:53:24 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -4,21 +4,14 @@ let d = new Date();
 let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 // Personal API Key and base url for OpenWeatherMap API.
-let apiKey = '&appid=8e2b6e81e6a1375344f684b58f2f8136&units=metric';
+const apiKey = 'Demo';
 let baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 
 
 // Event listener to add function to the generate button upon clicking.
 document.getElementById('generate').addEventListener('click', generate);
 
-/* Function generate() called by event listener. 
--defining the variables zipCode and feelings using .value property.
--Calling the async function (weather()) defined at line 38 with the parameters (baseURL, apiKey) defined globally and (zipCode) defined locally. 
--.then() method is used to execute the arrow function with the parameter (data) returned from the weather function.
--conditional statment to check and display an alert if there are any error messages received from the API, 
-if not, it defines the variables below fetched from the API and packages them in the object `gatheredData` to POST it to the server.
--calling the async POST request function (serverPost()) defined at line 55 to send the data to the server on the url extension /add.
--calling the async GET request function defined at line 71 to get and update the data in the user interface.*/
+/* Function generate() called by event listener.*/
 function generate(){
     const zipCode= document.getElementById('zip').value;
     const feelings= document.getElementById('feelings').value;
@@ -38,12 +31,7 @@ function generate(){
     });
 };
 
-
-
-/*Async function to GET Web API Data. 
--the function has 3 parameters (url, code, key) to build the API query.
--Defining the variable response that is fetched from the API using the await operator.
--Using Try and catch to define and return the variable 'data' (if resolved succesfully) after awaiting for parsing the body text as JSON using the .then() method.*/
+/*Async function to GET Web API Data. */
 const weather= async (url, code, key)=>{
     const response = await fetch(url+ code+ key);
     try {
@@ -54,16 +42,7 @@ const weather= async (url, code, key)=>{
     }
 };
 
-
-
-/*Async Function to POST gatheredData. 
--The function adds an entry to the project endpoint.
--First parameter is the URL to make the POST request to, and second parameter is the gatheredData object.
--method is set to POST because we are accessing the POST route setup in server.js.
--credentials and headers are boilerplate necessary for a successful POST request.
--Content-Type is set to json and the body option converts the object into a JSON string using the JSON.stringify()method.
-using try and catch to define and return the variable newData.
-*/
+//Async Function to POST gatheredData.
 const serverPost= async(url='',gatheredData={})=>{
     const response= await fetch(url, {
         method: 'POST',
@@ -78,8 +57,6 @@ const serverPost= async(url='',gatheredData={})=>{
         console.log('watch out', error);
     }
 };
-
-
 
 /*Async Function to GET Project Data and update user interface.
 -The function retrieves the data with a GET request to the /all path.
